7904f83bb68e560aea6ea61b1db34688873929b3
ZinedineMess <zinedinemessahel@hotmail.com>
2021-10-20 10:54:30 +0200

    build: ajout absolute path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './router/App';
+import 'index.css';
+import App from 'router/App';
 
 ReactDOM.render(
   <React.StrictMode>
