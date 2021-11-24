eb4e65587c25acbebff234fc2389d5e90de8ca50
ZinedineMess <zinedinemessahel@hotmail.com>
2021-11-24 17:34:46 +0100

    feat: add 404 error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import 'pages/Error/Error.css';
 
 const Error = () => {
     return (
-        <div>
-            salut
-        </div>
-    )
-}
+        <section className='error'>
+            <h1>404</h1>
+            <h3>We are sorry</h3>
+            <p>Page not found</p>
+        </section>
+    );
+};
 
 export default Error;
